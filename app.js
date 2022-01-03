4cb770a44ad89a3c95ab7ecc17d04c9264169143
Michael Bajor <michael.bajor@prutech.com>
2022-01-03 13:00:26 -0500

    add cors

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ require('dotenv').config();
 
 const
     express = require('express'),
-    NewsApiKit = require('./libs/newsApiKit');
+    cors = require('cors'),
+    NewsApiKit = require('./libs/newsApiKit'),
     app = express(),
     PORT = 4200;
 
+app.use(cors());
 
 app.get('/', (req, res) => res.send('WhatsNewsAPI root test'));
 
 app.get('/api/news/:query', (req, res) => {
-    console.log(`News API Route hit: ${new Date().toString()}`)
     const { params: { query }} = req;
+    console.log(`News API Route hit:    [${query}]      ${new Date().toString()}`);
 
+    if(!query) {
+        res.status((500)).send('no query');
+    }
     // todo: security-> need to clean query
 
     NewsApiKit.getEverythingNews(query)
-        .then(response => JSON.parse(response.text))
         .then(data => {
             // console.log(data);
-            res.send(data);
+            res.send(data.text);
         })
-        .catch(err => console.error(err));
+        .catch(error => {
+            console.error(error);
+            res.status(500).end(error)
+        });
 });
 
 app.listen(PORT, () => console.log(`Whats News API listening on port ${PORT} @ ${new Date().toString()}`));
